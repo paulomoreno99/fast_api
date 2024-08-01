a0cde176e5db082636c125c2faf6d1beaa524b8c
paulomoreno99 <paulom.info@gmail.com>
2024-08-01 04:25:14 -0300

    before_jwt_time-01:25

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,12 +1,15 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+
 from fast_api.database import get_session
 from fast_api.models import User
-from fast_api.schemas import Message, UserList, UserPublic, UserSchema
+from fast_api.schemas import Message, UserList, UserPublic, UserSchema, Token
+from fast_api.security import get_password_hash, verify_password, create_access_token, get_current_user
 
 app = FastAPI()
 
@@ -34,9 +37,11 @@ def create_user(user: UserSchema, session=Depends(get_session)):
                 status_code=HTTPStatus.BAD_REQUEST,
                 detail='Email already exists',
             )
+        
+    hashed_password = get_password_hash(user.password)
 
     db_user = User(
-        username=user.username, password=user.password, email=user.email
+        username=user.username, password=hashed_password, email=user.email
     )
     session.add(db_user)
     session.commit()
@@ -46,14 +51,16 @@ def create_user(user: UserSchema, session=Depends(get_session)):
 
 
 @app.get('/users/', response_model=UserList)
-def read_users(limit: int = 10,skip: int = 0,session:Session = Depends(get_session)):
+def read_users(limit: int = 10,skip: int = 0,session:Session = Depends(get_session),current_user=Depends(get_current_user)):
     users = session.scalars(select(User).limit(limit).offset(skip))
     
     return {'users': users}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema, session:Session = Depends(get_session)):
+def update_user(user_id: int, user: UserSchema, session:Session = Depends(get_session),current_user=Depends(get_current_user)):
+
+    
     db_user = session.scalar(select(User).where(User.id == user_id))
     if not db_user:
         raise HTTPException(
@@ -61,7 +68,7 @@ def update_user(user_id: int, user: UserSchema, session:Session = Depends(get_se
         )
 
     db_user.username = user.username
-    db_user.password = user.password
+    db_user.password = get_password_hash(user.password)
     db_user.email = user.email
     session.commit()
     session.refresh(db_user)
@@ -70,7 +77,7 @@ def update_user(user_id: int, user: UserSchema, session:Session = Depends(get_se
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session:Session = Depends(get_session)):
+def delete_user(user_id: int, session:Session = Depends(get_session),current_user=Depends(get_current_user)):
     db_user = session.scalar(select(User).where(User.id == user_id))
 
     if not db_user:
@@ -80,7 +87,29 @@ def delete_user(user_id: int, session:Session = Depends(get_session)):
 
     session.delete(db_user)
 
-
     session.commit()
 
     return {'message': 'User deleted'}
+
+@app.post('/token/', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password'
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password'
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
---
+++ fast_api/security.py
@@ -0,0 +1,66 @@
+from datetime import datetime, timedelta
+from http import HTTPStatus
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from zoneinfo import ZoneInfo
+from pwdlib import PasswordHash
+from jwt import DecodeError, decode, encode
+
+from fast_api.database import get_session
+from fast_api.models import User
+from fast_api.schemas import Token
+
+pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = 'your-secret-key'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def get_password_hash(password= str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    # Adiciona um tempo de 30 minutos para expiração do token
+    expire = datetime.now(tz=ZoneInfo('UTC') + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({'exp': expire})
+    encode_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if not username:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except DecodeError:
+        raise credentials_exception
+
+    user = session.scalar(
+        select(User).where(User.email == token_data.username)
+    )
+
+    if not user:
+        raise credentials_exception
+
+    return user
+
